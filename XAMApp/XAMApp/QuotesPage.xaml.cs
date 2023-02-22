2afab1e8e369011af497d769e342272aee6f8f67
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-02-22 10:23:10 +0530

    Indented code.

--- XAMApp/XAMApp/QuotesPage.xaml.cs
+++ XAMApp/XAMApp/QuotesPage.xaml.cs
@@ -16,17 +16,18 @@ namespace XAMApp
 	{
 		private int _index = 0;
 		private string[] _quotes = new string[] {
-		"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best. ― Marilyn Monroe",
-"You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth. ― William W. Purkey",
-"You only live once, but if you do it right, once is enough. ― Mae West",
-"In three words I can sum up everything I've learned about life: it goes on. ― Robert Frost",
-"To live is the rarest thing in the world. Most people exist, that is all. ― Oscar Wilde",
-"Insanity is doing the same thing, over and over again, but expecting different results. ― Narcotics Anonymous",
-"It is better to be hated for what you are than to be loved for what you are not. ― Andre Gide, Autumn Leaves",
-"There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle. ― Albert Einstein",
-"It does not do to dwell on dreams and forget to live. ― J.K. Rowling, Harry Potter and the Sorcerer's Stone",
-"Good friends, good books, and a sleepy conscience: this is the ideal life. ― Mark Twain",
-"Life is what happens to us while we are making other plans. ― Allen Saunders" };
+			"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best. ― Marilyn Monroe",
+			"You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth. ― William W. Purkey",
+			"You only live once, but if you do it right, once is enough. ― Mae West",
+			"In three words I can sum up everything I've learned about life: it goes on. ― Robert Frost",
+			"To live is the rarest thing in the world. Most people exist, that is all. ― Oscar Wilde",
+			"Insanity is doing the same thing, over and over again, but expecting different results. ― Narcotics Anonymous",
+			"It is better to be hated for what you are than to be loved for what you are not. ― Andre Gide, Autumn Leaves",
+			"There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle. ― Albert Einstein",
+			"It does not do to dwell on dreams and forget to live. ― J.K. Rowling, Harry Potter and the Sorcerer's Stone",
+			"Good friends, good books, and a sleepy conscience: this is the ideal life. ― Mark Twain",
+			"Life is what happens to us while we are making other plans. ― Allen Saunders" 
+		};
 
 		private string Quote { get; set; }
 
