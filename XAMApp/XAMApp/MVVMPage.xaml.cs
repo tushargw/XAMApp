b26c24d674a48a529e058305bbb344ed93f41f07
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-02-24 14:45:13 +0530

    Created MVVM based backend.

---
+++ XAMApp/XAMApp/MVVMPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace XAMApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MVVMPage : ContentPage
+	{
+		public MVVMPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ XAMApp/XAMApp/ViewModels/MVVMPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XAMApp.ViewModels
+{
+	internal class MVVMPageViewModel : INotifyPropertyChanged
+	{
+		public MVVMPageViewModel()
+		{
+			ClearCommand = new Command(() =>
+			{
+				Comments = string.Empty;
+			});
+			PostCommand = new Command(() =>
+			{
+				Result = Comments;
+				Comments = string.Empty;
+			});
+		}
+		string _Comments;
+		string _result;
+		public string Comments
+		{
+			get => _Comments;
+			set
+			{
+				_Comments = value;
+				var args = new PropertyChangedEventArgs(nameof(Comments));
+				PropertyChanged?.Invoke(this, args);
+			}
+		}
+		public string Result
+		{
+			get => _result;
+			set
+			{
+				_result = value;
+				var args = new PropertyChangedEventArgs(nameof(Result));
+				PropertyChanged?.Invoke(this, args);
+			}
+		}
+		public Command PostCommand { get; }
+		public Command ClearCommand { get; }
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
