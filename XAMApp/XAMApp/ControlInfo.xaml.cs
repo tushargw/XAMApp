e5b54473ad3aeed1e06a4a764865b01aef7d4236
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-02-24 12:05:30 +0530

    Added ControlInfo Page

---
+++ XAMApp/XAMApp/ControlInfo.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ControlInfo : ContentPage
+	{
+		public ControlInfo()
+		{
+			InitializeComponent();
+		}
+
+		private void Switch_Toggled(object sender, ToggledEventArgs e)
+		{
+			label.Text= e.Value.ToString();
+		}
+
+		private void ContactsMethod_SelectedIndexChanged_1(object sender, EventArgs e)
+		{
+			var response = ContactsMethod.Items[ContactsMethod.SelectedIndex];
+			DisplayAlert("Title", response, "Ok");
+		}
+	}
+}
