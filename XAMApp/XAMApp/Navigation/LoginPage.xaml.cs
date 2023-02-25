81a0bdea9135032619e69e1411f48167d54203f5
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-02-25 12:11:17 +0530

    Login Page completed.

---
+++ XAMApp/XAMApp/Navigation/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMApp.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			if (Username.Text == "admin" && Password.Text == "admin")
+			{
+				DisplayAlert("Success !!", "You have logged in successfully.", "Ok");
+				return;
+			}
+			DisplayAlert("Failure !!", "Username or password is incorrect.", "Retry");
+		}
+	}
+}
