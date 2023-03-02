9f4674b098d5f9a6a5a0aca844d9722bf83c6004
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-03-02 11:21:24 +0530

    Parent-Child Pub-Sub handling.

---
+++ XAMApp/XAMApp/Navigation/ChildPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMApp.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChildPage : ContentPage
+	{
+		public event EventHandler<double> SliderValueChanged;
+
+		public ChildPage()
+		{
+			InitializeComponent();
+		}
+
+		private void silder_ValueChanged(object sender, ValueChangedEventArgs e)
+		{
+			SliderValueChanged?.Invoke(this, e.NewValue);
+		}
+	}
+}
---
+++ XAMApp/XAMApp/Navigation/ParentPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMApp.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ParentPage : ContentPage
+	{
+		public ParentPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			var page = new ChildPage();
+			page.SliderValueChanged += Page_SliderValueChanged;
+			Navigation.PushAsync(page);
+		}
+		private void Page_SliderValueChanged(object sender, double e)
+		{
+			label1.Text = e.ToString();
+		}
+	}
+}
