7ea68d49fb9cc659b36ee7fe7fc63c6dccdd3dc9
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-03-02 14:19:51 +0530

    Styles Page sample

---
+++ XAMApp/XAMApp/StylesPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StylesPage : ContentPage
+	{
+		public StylesPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked_1(object sender, EventArgs e)
+		{
+			if (!Resources.ContainsKey("buttonBackGroundColor"))
+				Resources.Add("buttonBackGroundColor", (Color)App.Current.Resources["buttonBackGroundColor"]);
+
+			Resources["buttonBackGroundColor"] = ((Color)Resources["buttonBackGroundColor"]) == Color.Pink ? Color.Blue : Color.Pink;
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			if (!Resources.ContainsKey("DisabledButton"))
+				Resources.Add("DisabledButton", (Style)App.Current.Resources["DisabledButton"]);
+			((Button)sender).Style = ((Button)sender).Style.BaseResourceKey == "DisabledButton" ? (Style)App.Current.Resources["GoodButton"] : (Style)App.Current.Resources["DisabledButton"];
+		}
+	}
+}
