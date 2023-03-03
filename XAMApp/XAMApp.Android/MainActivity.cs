7826c7640a639af92a11feee60cd7eba0374b163
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-03-03 14:07:22 +0530

    Maps control implementation

--- XAMApp/XAMApp.Android/MainActivity.cs
+++ XAMApp/XAMApp.Android/MainActivity.cs
@@ -4,25 +4,65 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android;
 
 namespace XAMApp.Droid
 {
-    [Activity(Label = "XAMApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+	[Activity(Label = "XAMApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		const int RequestLocationId = 0;
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		readonly string[] LocationPermissions =
+		{
+	Manifest.Permission.AccessCoarseLocation,
+	Manifest.Permission.AccessFineLocation
+};
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			Xamarin.FormsMaps.Init(this, savedInstanceState);
+			LoadApplication(new App());
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		{
+			if (requestCode == RequestLocationId)
+			{
+				if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted))
+				{
+					// Permissions granted - display a message.
+				}
+				else
+				{
+					// Permissions denied - display a message.
+				}
+			}
+			else
+			{
+				Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+				base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			}
+		}
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+
+			if ((int)Build.VERSION.SdkInt >= 23)
+			{
+				if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+				{
+					RequestPermissions(LocationPermissions, RequestLocationId);
+				}
+				else
+				{
+					// Permissions already granted - display a message.
+				}
+			}
+		}
+	}
 }
---
+++ XAMApp/XAMApp/FormsMapPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+/*
+ * Map integration details: 
+ * ------------------------
+ * https://learn.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/map/
+ * 
+ * Keytool command run to create the debug store:
+ * ----------------------------------------------
+ * keytool.exe -list -v -keystore "C:\Users\[USERNAME]\AppData\Local\Xamarin\Mono for Android\debug.keystore" -alias androiddebugkey -storepass android -keypass android
+ * 
+ * SHA1: A5:E2:AD:22:E4:75:C7:62:84:36:61:8E:04:B1:D6:21:BC:2F:44:DA
+ * 
+ * Maps SDK API-Key: AIzaSyCkqLioMGO9P3cbJEc0NmzLyS2pmiTemjg
+*/
+
+namespace XAMApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FormsMapPage : ContentPage
+	{
+		public FormsMapPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
