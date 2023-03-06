d0781d0f2c4a7eaa7c647befeb6c0c417d138923
Tushar G. Walavalkar <tushargw-hd7@hotmail.com>
2023-03-06 12:03:58 +0530

    Assignment on Custom controls.

---
+++ XAMApp/XAMApp/CustomControls/StackCustomControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XAMApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMApp.CustomControls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StackCustomControl : StackLayout
+	{
+		private readonly StackViewModel _vm;
+
+		public StackCustomControl()
+		{
+			InitializeComponent();
+
+			BindingContext = _vm = new StackViewModel();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			Result.Text = _vm.Title + ": " +_vm.Description;
+		}
+	}
+}
---
+++ XAMApp/XAMApp/ViewModels/StackViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XAMApp.ViewModels
+{
+	public class StackViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private string _title;
+		public string Title
+		{
+			get { return _title; }
+			set
+			{
+				if (_title != value)
+				{
+					_title = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
+				}
+			}
+		}
+
+		private string _description;
+		public string Description
+		{
+			get { return _description; }
+			set
+			{
+				if (_description != value)
+				{
+					_description = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Description"));
+				}
+			}
+		}
+	}
+}
